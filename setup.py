827acf7888db1820742fcec8a771b81c3ad4eedc
vizav <vishwanathavin@gmail.com>
2018-06-29 11:33:11 +0530

    made it a package

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
-from setuptools import setup, Extension
+from distutils.core import setup
 
 setup(
-    name="hmmlearn",
-    install_requires = ["numpy", "pandas"],
-    maintainer='VIZAV',
-    maintainer_email='vishwanathavin@gmail.com',
-    packages=["dataFormatter"]
+    # Application name:
+    name="dataFormatter",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="vishwanathAV",
+    author_email="vishwanathavin@gmail.com",
+
+    # Packages
+    packages=["dataFormatter"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/dataFormatter_v010/",
+
+    #
+    # license="LICENSE.txt",
+    description="Changes the datatype of the variables and realigns it",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "pandas",
+    ],
 )
